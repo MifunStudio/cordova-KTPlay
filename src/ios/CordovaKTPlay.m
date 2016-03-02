e6d642e72903d1ba97e37297dff8025544b2a805
Zhang张政明 <zhang.zhengming@immomo.com>
2016-03-02 16:48:45 +0800

    updated

--- src/ios/CordovaKTPlay.m
+++ src/ios/CordovaKTPlay.m
@@ -16,25 +16,29 @@
 
 - (void)showLoginView:(CDVInvokedUrlCommand*)command
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [KTAccountManager showLoginView:NO success:^(KTUser * account) {
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        } failure:^(NSError *error) {
-            NSString *errMsg = [error localizedDescription];
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errMsg];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }];
-    });
+    [self.commandDelegate runInBackground:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [KTAccountManager showLoginView:NO success:^(KTUser * account) {
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            } failure:^(NSError *error) {
+                NSString *errMsg = [error localizedDescription];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errMsg];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }];
+        });
+    }];
 }
 
 - (void)showCommunity:(CDVInvokedUrlCommand*)command
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [KTPlay show];
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    });
+    [self.commandDelegate runInBackground:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [KTPlay show];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        });
+    }];
 }
 
 @end
